0b7ca3b3a463be9fa29b2a954bb04df096cff655
RamonGiovane <ramon.giovane@gmail.com>
2020-04-25 17:41:49 -0300

    Lesson 96: Using ListView

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -4,48 +4,52 @@ import '../models/transaction.dart';
 import "package:intl/intl.dart";
 
 class TransactionList extends StatelessWidget {
-  
   final List<Transaction> _transactions;
-  
+
   TransactionList(this._transactions);
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
-              children: _transactions.map((tr) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: utils.boldText(
-                            content: utils.monetaryVal(tr.value),
-                            size: 20,
-                            color: Colors.purple),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          utils.boldText(content: tr.title),
-                          utils.boldText(
-                              content: DateFormat('d MMM y').format(tr.date),
-                              color: Colors.grey[600]),
-                        ],
-                      )
-                    ],
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemCount: _transactions.length,
+        itemBuilder: (ctx, index) {
+          final tr = _transactions[index];
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 10,
                   ),
-                );
-              }).toList(),
-            );
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: utils.boldText(
+                      content: utils.monetaryVal(tr.value),
+                      size: 20,
+                      color: Colors.purple),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    utils.boldText(content: tr.title),
+                    utils.boldText(
+                        content: DateFormat('d MMM y').format(tr.date),
+                        color: Colors.grey[600]),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
